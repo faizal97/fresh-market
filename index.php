cf29424ceebff89728d93d3e5f566919a6114e2f
faizal97 <faizalardianputra@yahoo.co.id>
2018-01-15 21:33:16 +0700

    Update index.php dan menambahkan folder style

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 <html>
 <head>
 	<title>FRESH MARKET</title>
+	<!-- Project's Stylesheet -->
+	<link rel="stylesheet" type="text/css" href="/style/index.css">
 </head>
 <body>
-<h1>FRESH MARKET</h1>
+<h1 id='header'>FRESH MARKET</h1>
+<php echo "hai" ?>
 </body>
 </html>
