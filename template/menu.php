eab8456f94624c121d50826e8025bbf59f6a8abe
faizal97 <faizalardianputra@yahoo.co.id>
2018-02-13 19:36:24 +0700

    update image dan menu

--- template/menu.php
+++ template/menu.php
@@ -1,12 +1,27 @@
  <link rel="stylesheet" type="text/css" href="style/menu.css">
  <nav class="navbar navbar-inverse navbar-fixed-top">
+  <img id="header" src="images/navbar.png" width="100%">
   <div class="container-fluid">
     <ul class="nav navbar-nav list-menu">
-      <li><a href="#">FISH</a></li>
-      <li><a href="#">CRAB</a></li>
-      <li><a href="#">SHRIMP</a></li>
-      <li><a href="#">SQUID</a></li>
-      <li><a href="#"><input type="input" style="display: none" name="" id="input" class="form-control searchbox" value="" required="required" title="" placeholder="Search..."><span id="search-icon" class="glyphicon glyphicon-search"></span></a></li>
+     <li class="logo"><img src="images/logo.png" width="10%"></li>
+      <li><a href="#">CUMI</a></li>
+      <li><a href="#">IKAN</a></li>
+      <li><a href="#">KEPITING</a></li>
+      <li><a href="#">UDANG</a></li>
+       <li><div id="search" class="searchbox">
+       		<form id='form-search' action="" method="get"><div class="input-group"><input type="search" class="form-control" placeholder="Cari Ikan ...." style="width:400px">
+    		<div class="input-group-btn">
+    		<button class="btn btn-default searchbox-btn" type="submit">
+        	<i class="glyphicon glyphicon-search"></i>
+     		</button>
+   			</div>
+   			</div>
+ 			</form>
+ 			</div>
+ 	  <li><a href="#">LOGIN</a></li>
+ 	  <li><a href="#">DAFTAR</a></li>
+ 	</li>
+ </li>
     </ul>
   </div>
 </nav>
