75b41ecc624b1f1b5c0360a7518b82f897cf5ee0
faizal97 <faizalardianputra@yahoo.co.id>
2018-02-14 18:11:37 +0700

    update parallax dan slogan

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,5 @@
 <div class="parallax">
-	<div class="caption"><span class="border">WELCOME TO FRESH MARKET</span>
+	<div class="caption"><span class="border"><img src="images/slogan.png" width="50%"></span>
 	</div>
 	<span class="glyphicon glyphicon-chevron-down arrow-down"></span>
 	<!-- <script type="text/javascript" src="scripts/parallax.js"></script> -->
